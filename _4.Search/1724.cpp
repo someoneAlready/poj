8dbd8384e2a9052068c648daf899195e5abacfc5
cgangee <cgangee@gmail.com>
2013-04-22 21:13:31 +0800

    ..

--- _4.Search/1724.cpp
+++ _4.Search/1724.cpp
@@ -28,60 +28,92 @@ template <class T> void _checkmin(T &t, T x){if (t == -1 || x < t) t = x;}
 #define repd(i, a, b) for (int i=(a); i>=(b); --i)
 #define iter(v) __typeof((v).begin())
 #define foreach(it, v) for (iter(v) it = (v).begin(); it != (v).end(); it++)
-#define clr(ar,val) memset(ar, val, sizeof(ar))
+#define clr(a, b) memset(a, b, sizeof(a))
 #define pb push_back
 #define sz(a) ((int)(a).size())
 #define all(a) a.begin(), a.end()
 #define mid(x, y) ((x+y)/2)
 #define vp vector<P>
 #define itr iterator
-
-struct node{
-	int to, l, t;
-	node *next;
-};
+#define N 100
+#define M 10000
 
 struct E{
-	int to, w;
-	E(){}
-	E(int to, int w):to(to),w(w){}
+    int to, w;
+    E(){}
+    E(int to, int w):to(to), w(w){}
 };
 
-int i,j,k,m,n,l,t;
-node *a[N+10];
-node pool[M+10];
-vector<E> L[N+10], T[N+10];
-int ll[N+10], tt[N+10];
-int idx;
+struct P{
+    int to, l, t;
+    P(){}
+    P(int to, int l, int t):to(to),l(l),t(t){}
+};
 
-void add(int i, int j, int l, int t){
-	node *p=pool+idx++;
-	p->to=to, p->l=l, p->t=t, p->next=a[i];
-	a[i]=p;
-}
+int i,j,k,m,n,l,li,ti,t,ans;
+vector<E> vl[N+10], vt[N+10];
+vector<P> a[N+10];
+int dL[N+10], dT[N+10];
+bool v[N+10];
+mii mp[N+10];
 
 
 void dfs(int i, int t, int l){
-	for (node *p=a[i]; p; p=p->next){
+
+    if (t-dT[i]<0 || (ans!=-1 && l+dL[i]>=ans)) return;           
+    if (i==n){
+        _checkmin(ans, l); return;
+    }
+    
+	mii::itr it=mp[i].lower_bound(t);
+	if (it!=mp[i].end() && l>=it->second) return;
+
+	while(1){
+		mii::itr it=mp[i].upper_bound(t); 
+		if (it==mp[i].begin() || it==mp[i].end()) break;
+		it--;
+		if (it->second>=l) mp[i].erase(it);
+		else break;
 	}
-}
-
-void gao(vector<E> a[N+10], int d[N+10]){
-	queue<int> q;
 	
+	mp[i][t]=l;
+    
+        
+    rep(k, sz(a[i])){
+        P &p=a[i][k];
+        dfs(p.to, t-p.t, l+p.l);
+    }
 }
 
+void gao(vector<E> a[], int d[N+10]){
+    queue<int> q; clr(d, -1), clr(v, 0);
+    q.push(n), d[n]=0, v[n]=1;
+    
+    while (!q.empty()){
+        int i=q.front(); q.pop(), v[i]=false;
+        rep(k, sz(a[i])){
+            E &e=a[i][k];
+            if (d[e.to]==-1 || d[e.to]>d[i]+e.w){
+                d[e.to]=d[i]+e.w;
+                if (!v[e.to])
+                    v[e.to]=1, q.push(e.to);
+            }
+        }
+    }
+}
 
 int main(){
-	scanf("%d%d%d", &t, &n, &m);
-	repf(i, 1, n) a[i]=NULL;
-	while (m--) 
-		scanf("%d%d%d%d", &i, &j, &k, &l), add(i, j, k, l),
-		L[j].pb(E(i, k)), T[j].pb(E(i, l));
-	gao(L, ll), gao(T, tt);
-
-	dfs(1, t, 0);
-	return 0;
+    while (~scanf("%d%d%d", &t, &n, &m)){
+        repf(i, 1, n) a[i].clear(), vl[i].clear(), vt[i].clear(), mp[i].clear();
+        while (m--){
+            scanf("%d%d%d%d", &i, &j, &li, &ti);
+            a[i].pb(P(j, li, ti)), vl[j].pb(E(i, li)), vt[j].pb(E(i, ti));
+        }
+        gao(vl, dL), gao(vt, dT);
+        ans=-1;
+        
+        dfs(1, t, 0);
+        printf("%d\n", ans);
+    }
+    return 0;
 }
-
-
